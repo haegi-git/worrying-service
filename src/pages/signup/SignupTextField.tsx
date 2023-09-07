4da37103a51a6505c9b527c96681b5ba9c7fc7b9
haegi <b_haegi@naver.com>
2023-09-07 17:44:04 +0900

    파이어베이스 업데이트 및 콘솔 메세지 삭제

--- src/pages/signup/SignupTextField.tsx
+++ src/pages/signup/SignupTextField.tsx
@@ -1,89 +1,92 @@
-import { styled } from "styled-components"
-import { useAppDispatch, useAppSelector } from "../../stores/store"
-import { onChangeEmail, onChangeNickname, onChangePassword } from "../../stores/features/signupState/signupStateSlice"
+import { styled } from "styled-components";
+import { useAppDispatch, useAppSelector } from "../../stores/store";
+import {
+  onChangeEmail,
+  onChangeNickname,
+  onChangePassword,
+} from "../../stores/features/signupState/signupStateSlice";
 
-import SignupInput from "./SignupInput"
+import SignupInput from "./SignupInput";
 
 const Container = styled.div`
-    display: flex;
-    flex-direction: column;
-    margin-top:15px;
-    input{
-        margin-bottom: 15px;
-        margin-top:5px;
-        border: none;
-        border-bottom: 1px solid black;
-        padding: 10px;
-    }
-    label{
-        padding-left: 10px;
-    }
-    p{
-        margin-bottom: 10px;
-        padding-left: 10px;
-    }
-`
+  display: flex;
+  flex-direction: column;
+  margin-top: 15px;
+  input {
+    margin-bottom: 15px;
+    margin-top: 5px;
+    border: none;
+    border-bottom: 1px solid black;
+    padding: 10px;
+  }
+  label {
+    padding-left: 10px;
+  }
+  p {
+    margin-bottom: 10px;
+    padding-left: 10px;
+  }
+`;
 
-export default function SignupTextField(){
+export default function SignupTextField() {
+  const singupState = useAppSelector((state) => state.signupState);
 
-    const singupState = useAppSelector((state)=>state.signupState)
+  const dispatch = useAppDispatch();
 
-    const dispatch = useAppDispatch()
+  const handleSignupInput =
+    (field: string) => (e: React.ChangeEvent<HTMLInputElement>) => {
+      const value = e.target.value;
 
-    const handleSignupInput = (field: string) => (e: React.ChangeEvent<HTMLInputElement>) => {
-        const value = e.target.value;
-
-        switch (field) {
-            case 'email':
-                dispatch(onChangeEmail(value));
-                console.log(singupState.photoURL)
-                break;
-            case 'nickname':
-                dispatch(onChangeNickname(value));
-                break;
-            case 'password':
-                dispatch(onChangePassword(value));
-                break;
-            default:
-                break;
-        }
+      switch (field) {
+        case "email":
+          dispatch(onChangeEmail(value));
+          break;
+        case "nickname":
+          dispatch(onChangeNickname(value));
+          break;
+        case "password":
+          dispatch(onChangePassword(value));
+          break;
+        default:
+          break;
+      }
     };
 
-    return(
-        <Container>
-            <SignupInput
-                htmlFor="Email"
-                label="이메일을 입력하세요."
-                name="Email"
-                id="Email"
-                type="email"
-                placeholder="이메일을 입력하세요."
-                value={singupState.email}
-                onChange={handleSignupInput('email')}
-                minLength={6}
-            />
-            <SignupInput
-                htmlFor="Nickname"
-                label="닉네임을 입력하세요."
-                name="Nickname"
-                id="Nickname"
-                type="text"
-                placeholder="닉네임을 입력하세요."
-                value={singupState.nickname}
-                onChange={handleSignupInput('nickname')}
-                minLength={3}
-            />
-            <SignupInput
-                htmlFor="Password"
-                label="비밀번호를 입력하세요."
-                name="Password"
-                id="Password"
-                type="password"
-                placeholder="비밀번호를 입력하세요."
-                value={singupState.password}
-                onChange={handleSignupInput('password')}
-                minLength={6}
-            />
-        </Container>
-    )
+  return (
+    <Container>
+      <SignupInput
+        htmlFor="Email"
+        label="이메일을 입력하세요."
+        name="Email"
+        id="Email"
+        type="email"
+        placeholder="이메일을 입력하세요."
+        value={singupState.email}
+        onChange={handleSignupInput("email")}
+        minLength={6}
+      />
+      <SignupInput
+        htmlFor="Nickname"
+        label="닉네임을 입력하세요."
+        name="Nickname"
+        id="Nickname"
+        type="text"
+        placeholder="닉네임을 입력하세요."
+        value={singupState.nickname}
+        onChange={handleSignupInput("nickname")}
+        minLength={3}
+      />
+      <SignupInput
+        htmlFor="Password"
+        label="비밀번호를 입력하세요."
+        name="Password"
+        id="Password"
+        type="password"
+        placeholder="비밀번호를 입력하세요."
+        value={singupState.password}
+        onChange={handleSignupInput("password")}
+        minLength={6}
+      />
+    </Container>
+  );
 }
--- src/pages/write/WriteCreateBtn.tsx
+++ src/pages/write/WriteCreateBtn.tsx
@@ -1,89 +1,90 @@
-import { styled } from "styled-components"
+import { styled } from "styled-components";
 
 import { useAppDispatch, useAppSelector } from "../../stores/store";
 import { useNavigate } from "react-router-dom";
 import useWrite from "../../Hooks/useWrite";
-import { onChangeContent, onChangeTitle } from "../../stores/features/writeValue/writeValueSlice";
+import {
+  onChangeContent,
+  onChangeTitle,
+} from "../../stores/features/writeValue/writeValueSlice";
 
 const Container = styled.div`
-    width: 100%;
-    display: flex;
-    justify-content: end;
-    margin-top: 15px;
-    button{
-        width: 100px;
-        height: 30px;
-        border: 1px solid black;
-        border-radius: 15px;
-        background-color: #fff;
+  width: 100%;
+  display: flex;
+  justify-content: end;
+  margin-top: 15px;
+  button {
+    width: 100px;
+    height: 30px;
+    border: 1px solid black;
+    border-radius: 15px;
+    background-color: #fff;
+  }
+`;
+
+export default function WriteCreateBtn() {
+  const writeValue = useAppSelector((state) => state.writeValue);
+
+  const userState = useAppSelector((state) => state.userState);
+
+  const dispatch = useAppDispatch();
+
+  const createBtn = useWrite();
+
+  const timestamp = Date.now();
+
+  const navigate = useNavigate();
+
+  const handelCreateBtn = async () => {
+    if (writeValue.category === "null" || "") {
+      alert("카테고리를 선택 해주세요.");
     }
-`
 
-export default function WriteCreateBtn(){
-
-    const writeValue = useAppSelector((state)=>state.writeValue)
-
-    const userState = useAppSelector((state)=>state.userState)
-
-    const dispatch = useAppDispatch()
-
-    const createBtn = useWrite();
-
-    const timestamp = Date.now()
-
-    const navigate = useNavigate()
-    console.log(writeValue.category)
-
-    const handelCreateBtn = async () =>{
-        if(writeValue.category === 'null' || ''){
-            alert('카테고리를 선택 해주세요.')
-        }
-
-        if(!userState.userUid){
-            alert('로그인이 필요합니다.')
-        }
-        
-        if(writeValue.category === 'anonymous'){
-            await createBtn.postWrite({
-                writeValue: writeValue,
-                userState: userState,
-                date: timestamp,
-                postName: '작성자'})
-
-                dispatch(onChangeTitle(''))
-                dispatch(onChangeContent(''))
-
-            navigate(`/${writeValue.category}`)
-        }else if(writeValue.category === 'secret'){
-            await createBtn.postWrite({
-                writeValue: writeValue,
-                userState: userState,
-                date: timestamp,
-                postName: '작성자'})
-
-                dispatch(onChangeTitle(''))
-                dispatch(onChangeContent(''))
-
-            navigate(`/${writeValue.category}`)
-        }else if(writeValue.category === 'free'){
-            await createBtn.postWrite({
-                writeValue: writeValue,
-                userState: userState,
-                date: timestamp,
-                postName: userState.userNickname})
-
-                dispatch(onChangeTitle(''))
-                dispatch(onChangeContent(''))
-
-            navigate(`/${writeValue.category}`)
-        }
-
+    if (!userState.userUid) {
+      alert("로그인이 필요합니다.");
     }
-    return(
-        <Container>
-            <button onClick={handelCreateBtn}>
-                글 생성
-            </button>
-        </Container>
-    )
+
+    if (writeValue.category === "anonymous") {
+      await createBtn.postWrite({
+        writeValue: writeValue,
+        userState: userState,
+        date: timestamp,
+        postName: "작성자",
+      });
+
+      dispatch(onChangeTitle(""));
+      dispatch(onChangeContent(""));
+
+      navigate(`/${writeValue.category}`);
+    } else if (writeValue.category === "secret") {
+      await createBtn.postWrite({
+        writeValue: writeValue,
+        userState: userState,
+        date: timestamp,
+        postName: "작성자",
+      });
+
+      dispatch(onChangeTitle(""));
+      dispatch(onChangeContent(""));
+
+      navigate(`/${writeValue.category}`);
+    } else if (writeValue.category === "free") {
+      await createBtn.postWrite({
+        writeValue: writeValue,
+        userState: userState,
+        date: timestamp,
+        postName: userState.userNickname,
+      });
+
+      dispatch(onChangeTitle(""));
+      dispatch(onChangeContent(""));
+
+      navigate(`/${writeValue.category}`);
+    }
+  };
+  return (
+    <Container>
+      <button onClick={handelCreateBtn}>글 생성</button>
+    </Container>
+  );
 }
